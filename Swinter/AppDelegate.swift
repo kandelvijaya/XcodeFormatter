25f93d0afbe2a8e6fd3d453f1cbba0ff501778b7
kandelvijaya <kandelvijaya@gmail.com>
2016-09-15 20:56:58 +0200

    Start of Awesome Swift Linter

---
+++ Swinter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swinter
+//
+//  Created by Vijaya Prakash Kandel on 9/15/16.
+//  Copyright © 2016 Vijaya Prakash Kandel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swinter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swinter
+//
+//  Created by Vijaya Prakash Kandel on 9/15/16.
+//  Copyright © 2016 Vijaya Prakash Kandel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
