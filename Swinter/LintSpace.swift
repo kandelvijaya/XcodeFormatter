280d363e36685ea003d54444a2a1cec7d24a312d
kandelvijaya <kandelvijaya@gmail.com>
2016-09-15 22:28:45 +0200

    Refactored Colon Space Linter. All unit tests passing. Even for complex structure.

--- Swinter/LintSpace.swift
+++ Swinter/LintSpace.swift
@@ -10,39 +10,95 @@ import Foundation
 
 final class LintSpace {
     
-    let inconsistentColonSpaceRegex = ".(:)([^ ]|[ ]{2,})."
+    fileprivate enum SpaceMatchType {
+        case NoSpace
+        case MultipleSpace(spaceCount: Int)
+    }
+    
+    fileprivate struct SpaceMatchedRange {
+        let matchType: SpaceMatchType
+        let matchRange: Range<String.Index>
+    }
     
     func ensureOneSpaceAfterColon(line: String) -> String {
-        var lineString = line
+        guard let regex = regexForColonSpace() else { return line }
+        let matchedRanges = findAllMatchRanges(in: line, with: regex)
+        let correctedLine = correctColonSpace(for: line, at: matchedRanges)
+        return correctedLine
+    }
+    
+}
+
+//MARK:- Private methods
+
+extension LintSpace {
+
+    fileprivate func correctColonSpace(for lineString: String, at matchedRanges: [SpaceMatchedRange]) -> String {
+        var rangePositonOffset = 0
+        var correctedLine = lineString
+        
+        for range in matchedRanges {
+            
+            let newLowerIndex = correctedLine.index(range.matchRange.lowerBound, offsetBy: rangePositonOffset)
+            let newUpperIndex = correctedLine.index(range.matchRange.upperBound, offsetBy: rangePositonOffset)
+            let correctedRange = Range(uncheckedBounds: (newLowerIndex, newUpperIndex))
+            
+            switch range.matchType {
+            case .NoSpace:
+                rangePositonOffset += 1
+                correctedLine.insert(Character(" "), at: correctedRange.lowerBound)
+                
+            case .MultipleSpace( let spaceCount):
+                rangePositonOffset -= (spaceCount - 1) //1 for " "
+                correctedLine.replaceSubrange(correctedRange, with: " ")
+            }
+        }
+        
+        return correctedLine
+    }
+    
+    fileprivate func regexForColonSpace() -> NSRegularExpression? {
+        //NOTE:- Try it on www.regex101.com
+        let inconsistentColonSpaceRegex = ".(:)([^ ]|[ ]{2,})."
         
         do {
             let regex = try NSRegularExpression(pattern: inconsistentColonSpaceRegex, options: [])
-            
-            regex.enumerateMatches(in: lineString, options: .reportCompletion, range: NSMakeRange(0, lineString.characters.count), using: { (textCheckingResult, flags, status) in
-                
-                if let textChecking = textCheckingResult {
-                    let range = textChecking.rangeAt(2)
-                    let matchedString = (lineString as NSString).substring(with: range)
-                    if matchedString.contains(" ") {
-                        //matched more than 1 spaces
-                        lineString.insert(Character(" "), at: lineString.index(lineString.startIndex, offsetBy: range.location))
-                        let lowerIndex = lineString.index(lineString.startIndex, offsetBy: range.location)
-                        let upperIndex = lineString.index(lineString.startIndex, offsetBy: range.location + range.length)
-                        let subrange = ClosedRange(uncheckedBounds: (lowerIndex ,upperIndex))
-                        lineString.replaceSubrange(subrange, with: " ")
-                    } else {
-                        //There were no spaces in between
-                        let insertSpaceIndex = lineString.index(lineString.startIndex, offsetBy: range.location)
-                        lineString.insert(" ", at: insertSpaceIndex)
-                    }
-                }
-                
-            })
+            return regex
         } catch {
             print("Bad Regular Expression \(inconsistentColonSpaceRegex)")
+            return nil
         }
-        
-        return lineString
     }
     
+    fileprivate func findAllMatchRanges(in lineString: String, with regex: NSRegularExpression) -> [SpaceMatchedRange] {
+        var matchedRanges: [SpaceMatchedRange] = []
+        
+        regex.enumerateMatches(in: lineString, options: .reportCompletion, range: NSMakeRange(0, lineString.characters.count), using: { (textCheckingResult, flags, status) in
+            
+            if let textChecking = textCheckingResult {
+                let secondCaptureGroupRange = textChecking.rangeAt(2)
+                let matchedRange = rangeFrom(nsrange: secondCaptureGroupRange, forString: lineString)
+                let matchedString = lineString.substring(with: matchedRange)
+                
+                if matchedString.contains(" ") {
+                    //matched more than 1 spaces
+                    let spaceRange = SpaceMatchedRange(matchType: .MultipleSpace(spaceCount: matchedString.characters.count), matchRange: matchedRange)
+                    matchedRanges.append(spaceRange)
+                } else {
+                    //There were no spaces in between
+                    let spaceRange = SpaceMatchedRange(matchType: .NoSpace, matchRange: matchedRange)
+                    matchedRanges.append(spaceRange)
+                }
+            }
+            
+        })
+        return matchedRanges
+    }
+    
+    private func rangeFrom(nsrange: NSRange, forString: String) -> Range<String.Index> {
+        let lowerIndex = forString.index(forString.startIndex, offsetBy: nsrange.location)
+        let upperIndex = forString.index(forString.startIndex, offsetBy: nsrange.location + nsrange.length)
+        return Range(uncheckedBounds: (lowerIndex, upperIndex))
+    }
+
 }
--- SwinterTests/SpacerTests.swift
+++ SwinterTests/SpacerTests.swift
@@ -19,6 +19,15 @@ class SpacerTests: XCTestCase {
         XCTAssertEqual(expectedOutput, output)
     }
     
+    func testThat_ColonFollowedByMultipleSpace_IsTrimmed() {
+        let testInputLine = "let a:        String?"
+        let expectedOutput = "let a: String?"
+        
+        let output = LintSpace().ensureOneSpaceAfterColon(line: testInputLine)
+        
+        XCTAssertEqual(expectedOutput, output)
+    }
+    
     func testThat_ColonFollowedByMoreSpace_IsReplaced() {
         let testInputLines = ["let a:   Int", "let b:              String?", "[String:  AnyObject]"]
         let expectedOutputs = ["let a: Int", "let b: String?", "[String: AnyObject]"]
@@ -36,4 +45,21 @@ class SpacerTests: XCTestCase {
         XCTAssertEqual(inputs, outputs)
     }
     
+    func testhThat_MultipleVoilationInSingleLine_AreCorrected() {
+        let input = "func do(a:Int, b:     Int) -> Int {"
+        let expected = "func do(a: Int, b: Int) -> Int {"
+        
+        let output = LintSpace().ensureOneSpaceAfterColon(line: input)
+        
+        XCTAssertEqual(expected, output, "the output is \(output)")
+    }
+    
+    func testThat_MultipleComplexVoilationInSinleLine_AreCorrected() {
+        let input = "func structFromJSON(json:   [String:AnyObject], struct:SomeStruct, completion:(Bool->Void)) {"
+        let expected = "func structFromJSON(json: [String: AnyObject], struct: SomeStruct, completion: (Bool->Void)) {"
+        
+        let output = LintSpace().ensureOneSpaceAfterColon(line: input)
+        
+        XCTAssertEqual(expected, output)
+    }
 }
